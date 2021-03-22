4a5f66d89dfc93a23efc47ef699fd3e3815fcb65
lavegui <pab.gutier@gmail.com>
2021-03-22 17:26:02 +0100

    Add error middleware

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import expensesRouter from './src/routes/expenses.js'
+
+import HttpException from './src/application/exception/HttpException.js'
+import errorMiddleware from './src/middleware/error.middleware.js'
+
 dotenv.config()
 
 const app = express()
@@ -9,6 +13,12 @@ const port = Number(process.env.PORT)
 app.use(express.json())
 
 app.use('/expenses', expensesRouter)
+
+app.all('*', (req, res, next) => {
+    const err = new HttpException(404, 'Not Found')
+    next(err);
+})
+app.use(errorMiddleware)
 
 app.listen(port, () => {
   console.log(`🚀Waiting for requests at http://localhost:${port}`)
---
+++ src/middleware/error.middleware.js
@@ -0,0 +1,16 @@
+function errorMiddleware(error, req, res, next) {
+    let { status = 500, message, data } = error
+
+    console.log(`[Error] ${error}`)
+
+    error = {
+        type: 'error',
+        status,
+        message,
+        ...(data) && data
+    }
+
+    res.status(status).send(error)
+}
+
+export default errorMiddleware
