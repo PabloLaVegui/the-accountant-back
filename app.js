ef1b295419793e3009b69ae857498b960b115e81
lavegui <pab.gutier@gmail.com>
2021-03-22 13:14:45 +0100

    Add expenses routes file

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const expensesRouter = require('./src/routes/expenses')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/expenses', expensesRouter)
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`🚀Waiting for requests at http://localhost:${port}`)
 })
+
+module.exports = app
